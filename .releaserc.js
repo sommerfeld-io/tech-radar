28e508ddc04ea089fb5880ae3fd35f479a861878
sebastian <sebastian@sommerfeld.io>
2025-11-21 12:51:22 +0000

    fix: push all changed files

--- .releaserc.js
+++ .releaserc.js
@@ -33,7 +33,6 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'package-lock.json'],
         message:
           'refactor(release): bump version to ${nextRelease.version} [Actions Bot]'
       }
